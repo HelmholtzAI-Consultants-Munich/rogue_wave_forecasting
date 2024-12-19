df7ca121f9d1f985f69242f4dfcb9672a37367cf
lisa-sousa <lisa.barros.andrade.sousa@gmail.com>
2024-12-19 11:34:15 +0100

    rerun analysis

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -7,6 +7,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import pickle
+from collections import Counter
+
+from sklearn.metrics import balanced_accuracy_score, confusion_matrix, f1_score, mean_squared_error, r2_score
+from scipy.stats import spearmanr
+
 ############################################################
 ##### Utility Functions
 ############################################################
@@ -163,3 +169,97 @@ def plot_correlation_matrix(data, figsize=(5, 5), annot=True, labelsize=10):
         ax=ax,
         annot=annot,
     )
+
+
+def load_data(case, undersample, undersample_method):
+    # Load and unpack the data
+    with open(
+        f'../data/data_case{case}{f"_{undersample_method}_undersampled" if undersample else ""}.pickle', "rb"
+    ) as handle:
+        data = pickle.load(handle)
+
+    data_train, data_test = data
+    y_train = data_train["AI_10min"]
+    y_train_cat = data_train["AI_10min_cat"]
+    y_test = data_test["AI_10min"]
+    y_test_cat = data_test["AI_10min_cat"]
+
+    X_train = data_train.drop(columns=["AI_10min", "AI_10min_cat"])
+    X_test = data_test.drop(columns=["AI_10min", "AI_10min_cat"])
+
+    print("\nTraining dataset target distribution:")
+    print(Counter(data_train["AI_10min_cat"]))
+
+    print("\nTest dataset target distribution:")
+    print(Counter(data_test["AI_10min_cat"]))
+
+    return data_train, data_test, y_train, y_train_cat, X_train, y_test, y_test_cat, X_test
+
+
+def load_data_and_model(model_type, case, undersample_method, undersample):
+
+    # Load and unpack the data
+    with open(
+        f'../models/{model_type}_model_randomforest_case{case}{f"_{undersample_method}_undersampled" if undersample else ""}.pickle',
+        "rb",
+    ) as handle:
+        data_and_model = pickle.load(handle)
+
+    data_train, data_test, model = data_and_model
+
+    y_train = data_train["AI_10min"]
+    y_train_cat = data_train["AI_10min_cat"]
+    y_test = data_test["AI_10min"]
+    y_test_cat = data_test["AI_10min_cat"]
+
+    X_train = data_train.drop(columns=["AI_10min", "AI_10min_cat"])
+    X_test = data_test.drop(columns=["AI_10min", "AI_10min_cat"])
+
+    print("Training dataset target distribution:")
+    print(Counter(y_train_cat))
+
+    print("Test dataset target distribution:")
+    print(Counter(y_test_cat))
+
+    print(f"Loaded the following model: {model}")
+
+    if model_type == "class":
+        # Predict training labels
+        y_pred = model.predict(X_train)
+        y_true = y_train_cat
+
+        print(f"Balanced acc: {balanced_accuracy_score(y_true, y_pred)}")
+        print(f"Macro F1 score: {f1_score(y_true, y_pred, average='macro')}")
+        print(f"Confusion matrix:\n{confusion_matrix(y_true, y_pred)}")
+
+        # Predict test labels
+        y_pred = model.predict(X_test)
+        y_true = y_test_cat
+
+        print(f"Balanced acc: {balanced_accuracy_score(y_true, y_pred)}")
+        print(f"Macro F1 score: {f1_score(y_true, y_pred, average='macro')}")
+        print(f"Confusion matrix:\n{confusion_matrix(y_true, y_pred)}")
+
+    if model_type == "reg":
+
+        # Predict labels
+        y_pred = model.predict(X_train)
+        y_true = y_train
+
+        print(f"Train set MSE: {round(mean_squared_error(y_true, y_pred), 3)}")
+        print(f"Train set R^2: {round(r2_score(y_true, y_pred), 3)}")
+        print(f"Train set Spearman R: {round(spearmanr(y_true, y_pred).correlation, 3)}")
+
+        plot_predictions(y_true=y_true, y_pred=y_pred, textstr=f"$R^2={round(r2_score(y_true, y_pred), 3)}$")
+
+        # Predict labels
+        y_pred = model.predict(X_test)
+        y_true = y_test
+
+        print(f"Test set MSE: {round(mean_squared_error(y_true, y_pred), 3)}")
+        print(f"Test set R^2: {round(r2_score(y_true, y_pred), 3)}")
+        print(f"Test set Spearman R: {round(spearmanr(y_true, y_pred).correlation, 3)}")
+
+        plot_predictions(y_true=y_true, y_pred=y_pred, textstr=f"$R^2={round(r2_score(y_true, y_pred), 3)}$")
+
+    return model, X_train, y_train, y_train_cat, X_test, y_test, y_test_cat
